166b25bc0b8d048ff8e9c45d064f1fbc4ee198f2
Oluwabamigbe <oluwabunmiolaoluwa@gmail.com>
2021-03-12 13:25:27 +0100

    I don push tire

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMSEFDB
+{
+    public class Constants
+    {
+        public const string ConnectionString = "server=localhost;user=root;database=bmsef;port=3306;password=remember123";
+
+    }
+}
